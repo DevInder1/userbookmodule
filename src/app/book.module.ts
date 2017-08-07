3df154bbe234d063352bd9040b35152324eaa23c
Isingh <jattinder16@gmail.com>
2017-08-07 17:55:06 +0530

    update module

--- src/app/book.module.ts
+++ src/app/book.module.ts
@@ -1,3 +1,4 @@
+///<reference path="../../node_modules/@types/node/index.d.ts"/>
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {UserButtonComponent} from './user-book-component/user-button/user-button.component';
@@ -14,6 +15,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {Bookdetails} from './shared/services/bookdetails.service';
 import {SearchService} from './shared/services/search.service';
 import { NvD3Component } from 'ng2-nvd3';
+import { ChartModule } from 'angular2-highcharts';
+
 
 @NgModule({
   imports: [
@@ -21,6 +24,7 @@ import { NvD3Component } from 'ng2-nvd3';
     BrowserModule,
     FormsModule,
     HttpModule,
+    ChartModule.forRoot(require('highcharts')),
 
   ],
   declarations: [
