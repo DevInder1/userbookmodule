69e19c03c7405eedbe5a48b94d8be98380c26a9b
Isingh <jattinder16@gmail.com>
2017-08-08 18:12:45 +0530

    added routing with delete component

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -2,7 +2,8 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {ViewBooksComponent} from './user-book-component/view-books/view-books.component';
-import {UploadBooksComponent} from './user-book-component/upload-books/upload-books.component';
+import {UploadBooksComponent} from "./user-book-component/upload-books/upload-books.component";
+import {DeleteBooksComponent} from './user-book-component/delete-books/delete-books.component';
 
 
 const appRoutes: Routes = [
